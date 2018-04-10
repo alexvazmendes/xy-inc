2b025827f994aebaaac67417e61e2a0efff63eb2
alexvaz <alexvaz@algartelecom.com.br>
2018-04-09 22:14:06 -0300

    Ajustes nas props de teste e no Repos

--- src/main/java/br/com/xyinc/repository/PoiRepository.java
+++ src/main/java/br/com/xyinc/repository/PoiRepository.java
@@ -1,9 +1,9 @@
 package br.com.xyinc.repository;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import br.com.xyinc.model.Poi;
 
-public interface PoiRepository extends PagingAndSortingRepository<Poi, Long>{
+public interface PoiRepository extends CrudRepository<Poi, Long>{
 
 }
